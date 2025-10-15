f3e243fdb723880cab7aa17689bc8e3bfbdde121
alexnino8 <alopeznino98@gmail.com>
2025-10-14 19:56:35 -0600

    edited main.py, added video capture and grayscale conversion

--- main.py
+++ main.py
@@ -5,4 +5,28 @@ import cv2
 vision = cv2.VideoCapture(0)
 
 # la funcion read() permite convertir < cv2.VideoCapture 0x101e89690> en algo mas legible
-print(vision.read())
+while True:
+    # checar si se ha podido capturar la imagen
+    ret, frame = vision.read()
+    if not ret:
+        print("No se ha podido capturar la imagen")
+        break
+    # Esta variable condendrá la imagen capturada
+    captura = frame
+
+    # convertimos la imagen a escala de grises
+    captura_byn = cv2.cvtColor(captura, cv2.COLOR_BGR2GRAY)
+
+    # cv2 tiene una funcion que automaticamente construye la imagen pixel a pixel
+    cv2.imshow('Video tiempo real', clasificador_rostros) # image show -> imshow
+
+    # Tenemos que agregar una condicion de salida
+    # Salir con la tecla 'q'
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# suelta la camara para que otra aplicacion la pueda usar
+vision.release()
+
+# destruye todas las ventanas que se hayan creado
+cv2.destroyAllWindows()
